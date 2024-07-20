e5b68523a4a809b15c5f93c034806fa7a654ea2b
Shadi-Shwiyat <raz611103@gmail.com>
2024-07-20 15:21:10 -0500

    testing amplify changes

--- app/src/index.jsx
+++ app/src/index.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App/App.jsx';
-import StripeProvider from './Components/Stripe/StripeProvider.jsx';
+import Amplify from 'aws-amplify';
+import awsconfig from './aws-exports.js';
+// import StripeProvider from './Components/Stripe/StripeProvider.jsx';
 import './index.css';
+
+// Configure amplify for React App
+Amplify.congfigure(awsconfig);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <StripeProvider>
+    {/* <StripeProvider> */}
       <App />
-    </StripeProvider>
+    {/* </StripeProvider> */}
   </React.StrictMode>,
 )
