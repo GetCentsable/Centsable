c6b613e0b9c70c3c62164529c6e030955dbf22c1
Shadi-Shwiyat <raz611103@gmail.com>
2024-07-21 18:13:25 -0500

    updating aws to test lambda func

--- app/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ app/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -6,7 +6,32 @@ export type AmplifyDependentResourcesAttributes = {
       "RootUrl": "string"
     }
   },
+  "auth": {
+    "centsableb64cbd8e": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
   "function": {
+    "centsable8639d936CreateAuthChallenge": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    },
+    "centsable8639d936DefineAuthChallenge": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    },
     "findUserTest": {
       "Arn": "string",
       "LambdaExecutionRole": "string",
