e0cbaf6593ea2e218d6ff566d1eda165eee24569
Shadi-Shwiyat <raz611103@gmail.com>
2024-07-23 19:53:28 -0500

    fixed lambda function api request issue and able to now call func in front end

--- app/src/App/App.jsx
+++ app/src/App/App.jsx
@@ -1,12 +1,34 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from '../assets/react.svg'
 import viteLogo from '/vite.svg'
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [hello, setHello] = useState(0)
+  const [count, setCount] = useState(0);
+  const [hello, setHello] = useState('');
 
+  // Async function that test the lambda testFunction ()
+  async function testLambda() {
+    const url = 'https://fn3peb3590.execute-api.us-east-2.amazonaws.com/dev/centsable';
 
+    try {
+      const response = await fetch(url, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        }
+      });
+
+      if (response.status !== 200)  {
+        throw new Error(`HTTP request to /dev/centsable failed ${response.status}`);
+      }
+
+      const data = await response.json();
+      console.log(data);
+      setHello(data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  }
 
   return (
     <>
@@ -23,9 +45,8 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <button className='bg-red-500 hover:bg-lime-500 hover:cursor-pointer px-3 py-3' onClick={testLambda}>TEST LAMBDA</button>
+        <p className='text-center font-bold size-30 text-3xl'>{hello}</p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
--- app/src/index.jsx
+++ app/src/index.jsx
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App/App.jsx'
 import './index.css'
 
+// Amazon Amplify configuration integration
+import { Amplify } from 'aws-amplify';
+import awsconfig from './aws-exports.js';
+Amplify.configure(awsconfig);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
