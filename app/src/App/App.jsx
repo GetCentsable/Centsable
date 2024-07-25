6c4b85a9b742034bda45c4cfaf3c3c04aeb31620
Ben Harper <benjaminitonio@gmail.com>
2024-07-25 11:48:42 -0500

    Added Auth
    - Configured Cognito for AWS
    - Added Authenticator to App.jsx

--- app/src/App/App.jsx
+++ app/src/App/App.jsx
@@ -1,20 +1,30 @@
-import { useEffect, useState } from 'react'
-import Auth from '../Auth/Auth'
+// import { useEffect, useState } from 'react'
 import Dashboard from '../Dashboard/Dashboard'
+import { Authenticator } from '@aws-amplify/ui-react';
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(true);
-  const [userUsername, setUserUsername] = useState('');
+  // const [isLoggedIn, setIsLoggedIn] = useState(true);
+  // const [userUsername, setUserUsername] = useState('');
 
   return (
     <>
-      <div>
-        {isLoggedIn ? (
-          <Dashboard userUsername={userUsername} setIsLoggedIn={setIsLoggedIn} />
-        ) : (
-          <Auth setUserUsername={setUserUsername} setIsLoggedIn={setIsLoggedIn} />
-        )}
-      </div>
+      <Authenticator>
+      {({ signOut, user }) => (
+        <div className="App">
+          <header className="App-header">
+            <Dashboard userUsername={user.username} />
+            <button onClick={signOut}>Sign Out</button>
+          </header>
+        </div>
+      )}
+        {/* <div>
+          {isLoggedIn ? (
+            <Dashboard userUsername={userUsername} setIsLoggedIn={setIsLoggedIn} />
+          ) : (
+            <Auth setUserUsername={setUserUsername} setIsLoggedIn={setIsLoggedIn} />
+          )}
+        </div> */}
+      </Authenticator>
     </>
   )
 }
