7eca3bab56dba27b2fa95b03aa1741d48fa7e325
Shadi-Shwiyat <raz611103@gmail.com>
2024-07-28 18:36:39 -0500

    added functions index.js to export

--- app/functions/index.js
+++ app/functions/index.js
@@ -18,3 +18,16 @@ const logger = require("firebase-functions/logger");
 //   logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+
+// Import the functions from the other files
+const { signup, signin, verifyToken, verifyEmail, resetPassword, updatePassword, signout} = require('./authenticationFunctions');
+
+// Export the authentication functions to be used
+exports.signup = signup;
+exports.signin = signin;
+exports.verifyToken = verifyToken;
+exports.verifyEmail = verifyEmail;
+exports.resetPassword = resetPassword;
+exports.updatePassword = updatePassword;
+exports.signout = signout;
