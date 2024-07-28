4787fc23abe65c9a19cc31bee8bba32581fdaf96
Shadi-Shwiyat <raz611103@gmail.com>
2024-07-28 18:07:02 -0500

    removed amplify, fixed firebase.json routing for hosting

--- app/src/App/App.jsx
+++ app/src/App/App.jsx
@@ -1,6 +1,5 @@
 // import { useEffect, useState } from 'react'
 import Dashboard from '../Dashboard/Dashboard'
-import { Authenticator } from '@aws-amplify/ui-react';
 
 function App() {
   // const [isLoggedIn, setIsLoggedIn] = useState(true);
@@ -8,23 +7,7 @@ function App() {
 
   return (
     <>
-      <Authenticator>
-      {({ signOut, user }) => (
-        <div className="App">
-          <header className="App-header">
-            <Dashboard userUsername={user.username} />
-            <button onClick={signOut}>Sign Out</button>
-          </header>
-        </div>
-      )}
-        {/* <div>
-          {isLoggedIn ? (
-            <Dashboard userUsername={userUsername} setIsLoggedIn={setIsLoggedIn} />
-          ) : (
-            <Auth setUserUsername={setUserUsername} setIsLoggedIn={setIsLoggedIn} />
-          )}
-        </div> */}
-      </Authenticator>
+      <Dashboard></Dashboard>
     </>
   )
 }
--- app/src/index.jsx
+++ app/src/index.jsx
@@ -3,11 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App/App'
 import './index.css'
 
-// Amazon Amplify configuration integration
-import { Amplify } from 'aws-amplify';
-import awsconfig from './aws-exports.js';
-Amplify.configure(awsconfig);
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
