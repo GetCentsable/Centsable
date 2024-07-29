722143e92edd14b79abc689e721c3022689df8b3
Shadi-Shwiyat <raz611103@gmail.com>
2024-07-29 12:22:00 -0500

    stashing changes to app

--- app/src/index.jsx
+++ app/src/index.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App/App'
 import './index.css'
+// Plaid provider context for global plaid variables
+import { PlaidProvider } from "./Context/PlaidContext.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <PlaidProvider>
+      <App />
+    </PlaidProvider>
   </React.StrictMode>,
 )
