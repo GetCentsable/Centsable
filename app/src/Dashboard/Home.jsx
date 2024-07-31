36e0c949f604e11a1806750b9de68d7b9fad4b3a
F-O-P <tayler.coon@gmail.com>
2024-07-31 15:14:31 -0500

    adding typewriting to landing page

--- app/src/Dashboard/Home.jsx
+++ app/src/Dashboard/Home.jsx
@@ -1,15 +1,14 @@
+import Header from '../Components/General/Header';
+
+
 const HomePage = () => {
   return (
-    <div>
-      <div className="impact-section">
-        <h1 className="text-center">Impact</h1>
-      </div>
-      <div className="contributions-section">
-        <h1 className="text-center">Contributions</h1>
-      </div>
-      <div className="discover-section">
-        <h1 className="text-center">Discover</h1>
-      </div>
+    <div className="p-6">
+      <Header 
+        pageTopic="Your Impact"
+        icon="plus"
+        buttonText="View All"
+      />
     </div>
   );
 };
