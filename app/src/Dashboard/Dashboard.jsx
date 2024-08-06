c3460dc5e0fc7adf56413ad98e96e5a8acaa95da
Shadi-Shwiyat <raz611103@gmail.com>
2024-08-05 21:37:41 -0500

    implemented firefunction to load all user transactions from plaid API and store them in db

--- app/src/Dashboard/Dashboard.jsx
+++ app/src/Dashboard/Dashboard.jsx
@@ -1,18 +1,93 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import NavBar from '../Navigation/NavBar';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Routes, Route } from 'react-router-dom';
+import { app } from "../Firebase/firebase.js";
+import { getAuth } from 'firebase/auth';
 import Home from './Home';
 import Search from './Search';
 import Donations from './Donations';
 import Accounts from './Accounts';
 import UserDrawer from '../Components/General/UserDrawer';
+import TransactionContext from '../Context/TransactionsContext';
+import { translations } from '@aws-amplify/ui-react';
 
 const Dashboard = () => {
   const [isNavBarOpen, setIsNavBarOpen] = useState(true);
   const [isUserDrawerOpen, setIsUserDrawerOpen] = useState(false);
   const [selectedNavItem, setSelectedNavItem] = useState('Home');
   const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
+  const {
+    transactions,
+    setTransactions,
+    setTransactionsLoaded,
+  } = useContext(TransactionContext);
+
+  // Insantiate firebase auth
+  const auth = getAuth(app);
+
+  // Runs once on user log in to retrieve recent
+  // transactions from plaid api
+  useEffect(() => {
+    // Async function sends req to firebase function
+    const getUserTransactions = async () => {
+      // Path to the firebase function
+      const path = '';
+
+      try {
+        // Get current user object from firebase auth
+        const currentUser = auth.currentUser;
+
+        if(currentUser) {
+          // Generate firebase user id token from current user
+          const idToken = await currentUser.getIdToken();
+
+          // Fetch call to getUserTransactions to get non
+          // sensitive transaction data
+          const response = await fetch(path, {
+            method: "POST",
+            headers: {
+              'Content-Type': 'application/json',
+              'Authorization': `Bearer ${idToken}`,
+            },
+            body: JSON.stringify({
+              uid: currentUser.uid,
+            }),
+          });
+
+          // If request failed, reset transactions state to empty array,
+          // tranactionsLoaded state to false, and return
+          if (!response.ok) {
+            setTransactions([])
+            setTransactionsLoaded(false);
+            const errorData = response.json();
+            throw new Error(`HTTP error! status: ${response.status}, message: ${errorData.error}`);
+          }
+
+          // Check the data for errors
+          const data = await response.json();
+          if (!data) {
+            setTransactions([])
+            setTransactionsLoaded(false);
+            throw new Error(`HTTP error! status: ${response.status}`);
+          }
+
+          // If there are no errors, add the transactions array
+          // to the context transactions, and toggle transactionsLoaded
+          setTransactions(data.transactions);
+          setTransactionsLoaded(true);
+          console.log('User transactions retrieved:', transactions);
+        } else {
+          throw new Error('No current user found');
+        }
+      } catch (err) {
+        console.log('There was an error fetching user transaction history:', err);
+      }
+    }
+
+    // Invoke function
+    getUserTransactions();
+  }, []);
 
   useEffect(() => {
     const handleResize = () => setIsMobile(window.innerWidth < 768);
@@ -42,7 +117,6 @@ const Dashboard = () => {
         <main className={`
           flex-1 overflow-x-hidden overflow-y-auto bg-gray-100 transition-all duration-300 min-h-screen
           ${isMobile ? 'mt-16' : (isNavBarOpen ? 'md:ml-64' : 'md:ml-20')}
-          ${isMobile && isNavBarOpen ? 'pt-16' : ''}
         `}>
           <Routes>
             <Route path="/" element={<Home isUserDrawerOpen={isUserDrawerOpen} setSelectedNavItem={setSelectedNavItem} />} />
